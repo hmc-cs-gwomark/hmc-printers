ff07be977df752163af8c41d9a5a21fcc936f1bb
Hmc-cs-tdubno <tdubno@cs.hmc.edu>
2017-09-18 16:13:29 -0700

    added db interaction

--- app/controllers/printer_controller.rb
+++ app/controllers/printer_controller.rb
@@ -5,4 +5,16 @@ class PrinterController < ApplicationController
   def new
     @printer = Printer.new
   end
+  def update
+    @printer= Printer.where(:ids => params[:ids])
+    if @printer.update(printer_params)
+       redirect_to '/printers'
+     else
+       render 'index'
+     end
+  end
+  private
+    def printer_params
+      params.require(:printer).permit(:name,:ids,:status,:issue)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'statuses/index'
-  get '/printers' => 'printer#index'
+  get '/printers' => 'printer#index', as: :printer
+  post 'printers'=> 'printer#update'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
