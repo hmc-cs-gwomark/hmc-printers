98648de71eb7967d95cf62220d86cea5e228ffb1
hmc-cs-gwomark <gwomark@cs.hmc.edu>
2017-09-19 23:14:38 -0700

    added Jquery to remove description option when printer is working

--- app/controllers/printer_controller.rb
+++ app/controllers/printer_controller.rb
@@ -2,9 +2,11 @@ class PrinterController < ApplicationController
   def index
     @printers = Printer.all
   end
+
   def new
     @printer = Printer.new
   end
+
   def update
     @printer= Printer.where(:ids => params[:ids])
     if @printer.update(printer_params)
@@ -13,6 +15,11 @@ class PrinterController < ApplicationController
        render 'index'
      end
   end
+  
+  # def create
+  #   @printer = Printer.new(printer_params)
+  # end
+
   private
     def printer_params
       params.require(:printer).permit(:name,:ids,:status,:issue)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'statuses/index'
   get '/printers' => 'printer#index', as: :printer
   post 'printers'=> 'printer#update'
+  get '' => 'printer#index'
+  # get '/printers/new' => 'printer#new'
+  # post 'printers/new' => 'printer#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
