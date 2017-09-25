a50ab31350386b4e71d932e94e10055c67b5aec7
Hmc-cs-tdubno <tdubno@cs.hmc.edu>
2017-09-24 21:41:39 -0700

    made interface with status

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,6 +1,6 @@
 module StatsHelper
   def stats_make(id,status,issue)
-    @st = Stat.create(:ids => id,:status=>status,:issue=>issue)
+    @st = Stat.new(:ids => id,:status=>status,:issue=>issue)
     @st.save
     #Printer.remove(:ids=>"sup",:name=>"sup",:status=>"hello",:issue=>"yes")
     p "something tried"
